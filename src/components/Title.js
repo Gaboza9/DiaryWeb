c08dbf189973d9bf43b957f530d1d6af76694e4f
mmihye <smilehye910@naver.com>
2022-11-15 00:06:54 +0900

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function title() {
+  return (
+    <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Diary#</Navbar.Brand>
+          <Nav className="mb-3">
+            <Nav.Link href="#monthly">Monthly</Nav.Link>
+            <Nav.Link href="#weekly">Weekly</Nav.Link>
+            <Nav.Link href="#daily">Daily</Nav.Link>
+            <Nav.Link href="#review">Review</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+  )
+}
+
+export default title
