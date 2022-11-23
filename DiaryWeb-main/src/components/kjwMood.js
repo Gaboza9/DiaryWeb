dfd32379c8d4e5395766b2067c1d65d0c0b29808
kimgwon <92065911+kimgwon@users.noreply.github.com>
2022-11-23 21:29:47 +0900

    merge 전

--- DiaryWeb-main/src/components/kjwMood.js
+++
@@ -1,8 +0,0 @@
-function KjwMood(){
-    return (
-        <div className="mood">
-            오늘의 기분이 아직 선택되지 않았어요.
-        </div>
-    );
-}
-export default KjwMood;
--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Title from './components/Title';
-import Daily from './components/daily';
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Title from "./components/Title";
+import Daily from "./pages/daily";
+import WeeklyPage from "./pages/weekly/WeeklyPage";
 
 function App() {
   return (
-    <div className='App'>
-      <Title/>
+    <div className="App">
+      <Title />
       <BrowserRouter>
         <Routes>
           <Route path="/daily" element={<Daily />} />
+          <Route path="/weekly" element={<WeeklyPage />} />
         </Routes>
       </BrowserRouter>
     </div>
