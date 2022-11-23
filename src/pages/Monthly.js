835d26520820283c1c14bf2a23bfc9cec6d8b1b4
mmihye <smilehye910@naver.com>
2022-11-23 21:35:02 +0900

    monthly 페이지 연결

--- src/pages/Monthly.js
+++ src/pages/Monthly.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar';
+import MyCalendar from '../components/MyCalendar';
 
 function Monthly() {
     return (
         <>
-        <Title/>
         <div style={{display:'flex', flexDirection:'row', height:'100%'}}>
         <Sidebar/>
            <MyCalendar/>
