d4257804d6ed6d1bf865e01c731e53939f585856
mmihye <smilehye910@naver.com>
2022-11-24 12:43:17 +0900

    이미지 삭제

--- src/components/Menu/LocalImage.js
+++ src/components/Menu/LocalImage.js
@@ -6,10 +6,39 @@ function LocalImage(key) {
     const id = localStorage.getItem(key.days+"Id");
     const icon = localStorage.getItem(key.days+"Icon");
 
+    const useConfirm = (message = null, onConfirm, onCancel) => {
+        if (!onConfirm || typeof onConfirm !== "function") {
+          return;
+        }
+        if (onCancel && typeof onCancel !== "function") {
+          return;
+        }
+      
+        const confirmAction = () => {
+          if (window.confirm(message)) {
+            onConfirm();
+          } else {
+            onCancel();
+          }
+        };
+      
+        return confirmAction;
+      };
+      const deleteConfirm= () => { localStorage.removeItem(key.days+"Url");
+      localStorage.removeItem(key.days+"Id");
+      localStorage.removeItem(key.days+"Icon");
+        window.location.reload();};
+      const cancelConfirm = () => console.log("취소했습니다.");
+      const confirmDelete = useConfirm(
+        "삭제하시겠습니까?",
+        deleteConfirm,
+        cancelConfirm
+      );
+
     console.log(icon);
     if(url){
         return (
-            <div class="text-center">
+            <div class="text-center" onContextMenu={confirmDelete}>
             <Picture url={require(`../../img/${url}.png`)} id={id} />
             </div>
         )
