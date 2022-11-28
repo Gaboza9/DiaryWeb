51bbab6b14d943c05461c7b4bfa9d4b7d19ab810
mmihye <smilehye910@naver.com>
2022-11-28 18:47:38 +0900

    리뷰페이지 제목 추가

--- src/pages/BookReviewPage.js
+++ src/pages/BookReviewPage.js
@@ -3,6 +3,7 @@ import ReviewContents from "../components/ReviewContents";
 function BookReviewPage() {
   return (
     <>
+      <h1 class="text-center mt-4">책 리뷰 페이지</h1>
       <ReviewContents type="작가"/>
     </>
   );
--- src/pages/MovieReviewPage.js
+++ src/pages/MovieReviewPage.js
@@ -3,6 +3,7 @@ import ReviewContents from "../components/ReviewContents";
 function MovieReviewPage() {
   return (
     <>
+     <h1 class="text-center mt-4">영화 리뷰 페이지</h1>
       <ReviewContents type="배우"/>
     </>
   );
