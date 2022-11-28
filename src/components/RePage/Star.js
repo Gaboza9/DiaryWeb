4ec45e7515e35366c637827c46f7dba0c870cf7a
aneunoun <ththgy@naver.com>
2022-11-28 19:22:37 +0900

    별크기

--- src/components/RePage/Star.js
+++ src/components/RePage/Star.js
@@ -2,6 +2,7 @@ import { FaStar } from "react-icons/fa";
 
 const Star = ({ selected = false, onSelect = f => f }) => (
     <FaStar 
+        class="fs-4"
         color={selected ? "red" : "grey"}
         onClick={onSelect}
     />
